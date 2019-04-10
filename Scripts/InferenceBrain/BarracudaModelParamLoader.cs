4ec0c2c610865423b36fda810da97f2af5711484
Sebastian Höffner <info@sebastian-hoeffner.de>
2019-04-11 01:05:39 +0200

    Resolving Unity-Technologies/ml-agents#1900 using Unity-Technologies/ml-agents#1869

--- Scripts/InferenceBrain/BarracudaModelParamLoader.cs
+++ Scripts/InferenceBrain/BarracudaModelParamLoader.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Runtime.Remoting.Messaging;
 using Barracuda;
 using UnityEngine;
 using Tensor = MLAgents.InferenceBrain.Tensor;
